af38b8e09199ed513a1d324eda539ef5fab57c0d
James <me@j4m3s.eu>
2022-01-03 23:34:03 +0100

    feat(reshipi): add link to add ingredient to menu

--- reshipi/frontend/front/src/App.vue
+++ reshipi/frontend/front/src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/"> Liste de recettes </router-link>
-      <router-link to="/ustensils"> Liste d'ustensiles </router-link>
-      <router-link to="/ingredients"> Liste d'ingrédients </router-link>
+      <router-link to="/"> Liste de recettes </router-link> &nbsp;
+      <router-link to="/ustensils"> Liste d'ustensiles </router-link> &nbsp;
+      <router-link to="/ingredients"> Liste d'ingrédients </router-link> &nbsp;
+      <router-link to="/ingredient-edit"> Rajouter un ingrédient </router-link> &nbsp;
     </div>
     <router-view/>
   </div>
