9e93dfa0fb7dbfb8d618868f94f926e7e5f145b6
James <me@j4m3s.eu>
2022-02-05 15:42:24 +0100

    feat(reshipi): finish bare bones recipe edition

--- reshipi/frontend/front/src/components/RecipeEdition.vue
+++ reshipi/frontend/front/src/components/RecipeEdition.vue
@@ -50,6 +50,14 @@
             </li>
         </ul>
         <!-- Steps -->
+        <h2> Étapes </h2>
+        <ul>
+            <li v-for="(step, i) in steps" v-bind:key="i">
+                <input type="text" v-model="steps[i]" />
+                <button @click="removeStep(i)" > - </button>
+            </li>
+            <button @click="addStep" > Ajout d'une nouvelle étape </button>
+        </ul>
         <button v-on:click="createRecipe"> Créer la recette </button>
     </div>
     <div v-else>
@@ -75,10 +83,18 @@ export default Vue.extend({
         ingredients: [],
         ingredients_searched: [],
         ingredient_search_name: "",
+        steps: [],
+        steps_id: [],
         posted: false
     }
   },
   methods: {
+      addStep () {
+          this.steps.push("")
+      },
+      removeStep (index) {
+          this.steps.splice(index, 1)
+      },
       removeFromUstensils (index) {
           console.log(index)
           console.log(this.ustensils)
@@ -148,28 +164,82 @@ export default Vue.extend({
       createRecipe () {
           axios.post("http://localhost:3000/recipes", {
               name: this.name,
+          }).catch((error) => {
+              console.log(error)
+          })
+
+          axios.get(`http://localhost:3000/recipes?name=eq.${this.name}`, {
+              name: this.name,
           }).then((response) => {
+              console.log("getponse")
               console.log(response.data)
+              let recipe_id = response.data[0].id
 
               this.ustensils.map((ustensil) => {
-              axios.post("localhost:3000/ustensils_recipe", {
-                  ustensil_id: ustensil.id,
-                  recipe_id: response.data.id
-              }).then((response) => {
-                  console.log(response)
-              }).catch((error) => {
-                  console.log(error)
+                  axios.post("http://localhost:3000/ustensils_recipe", {
+                      ustensil_id: ustensil.id,
+                      recipe_id: recipe_id
+                  }).then((response) => {
+                      console.log(response)
+                  }).catch((error) => {
+                      console.log(error)
+                  })
               })
-          })
+
+
+              this.ingredients.map((ingredient) => {
+                  axios.post("http://localhost:3000/ingredients_recipe", {
+                      ingredient_id: ingredient.id,
+                      recipe_id: recipe_id
+                  }).then((response) => {
+                      console.log(response)
+                  }).catch((error) => {
+                      console.log(error)
+                  })
+              })
+
+              this.steps.map((step) => {
+                  console.log(`Step : ${step}`)
+                  axios.post("http://localhost:3000/steps", {
+                      text: step,
+                      recipe_id: recipe_id
+                  }).catch((error) => {
+                      console.log(error)
+                  })
+              })
+
+              let position = 0
+              this.steps_id.map((step) => {
+                      axios.post("http://localhost:3000/steps_recipe", {
+                          step_id: step,
+                          recipe_id: recipe_id,
+                          position: position
+                  }).then((response) => {
+                      position++
+                      console.log(response)
+                  }).catch((error) => {
+                      console.log(error)
+                  })
+              })
 
           }).catch((error) => {
               console.log(error)
               return
           })
 
-                },
+          this.posted = true
+      },
       reset () {
           this.name = ""
+          this.posted = false
+          this.ustensils = []
+          this.ustensils_searched = []
+          this.ustensil_search_name = ""
+          this.ingredients = []
+          this.ingredients_searched = []
+          this.ingredient_search_name = ""
+          this.steps = []
+          this.steps_id = []
           this.posted = false
       }
   }
