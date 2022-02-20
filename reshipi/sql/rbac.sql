46166d44cce1b98bff22c329c35ac0a1bd44b902
James <me@j4m3s.eu>
2022-02-20 17:05:37 +0100

    feat: add support for authenticated users

--- reshipi/sql/rbac.sql
+++ reshipi/sql/rbac.sql
@@ -3,23 +3,17 @@
 create role web_anon nologin;
 
 grant usage on schema api to web_anon;
-grant all on api.recipes to web_anon;
 grant usage, select on api.recipes_id_seq to web_anon;
-grant all on api.ingredients to web_anon;
 grant usage, select on api.ingredients_id_seq to web_anon;
-grant all on api.ingredients_recipe to web_anon;
 grant usage, select on api.ingredients_recipe_recipe_id_seq to web_anon;
 grant usage, select on api.ingredients_recipe_ingredient_id_seq to web_anon;
 
 grant select on api.ustensils to web_anon;
-grant all on api.ustensils to web_anon;
 grant select, usage on api.ustensils_id_seq to web_anon;
 grant select on api.ustensils_recipe to web_anon;
-grant all on api.ustensils_recipe to web_anon;
 grant select, usage on api.ustensils_recipe_recipe_id_seq to web_anon;
 grant select, usage on api.ustensils_recipe_ustensil_id_seq to web_anon;
 
-grant all on api.steps to web_anon;
 grant usage, select on api.steps_id_seq to web_anon;
 grant usage, select on api.steps_position_seq to web_anon;
 
@@ -27,9 +21,23 @@ create role authenticator noinherit login password 'mysecretpassword';
 grant web_anon to authenticator;
 
 --- authenticated users
-create role authenticated_user nologin;
-grant authenticated_user to authenticator;
+create role normal_user nologin;
+grant normal_user to authenticator;
 
-grant usage on schema api to authenticated_user;
-grant all on api.recipes to authenticated_user;
-grant usage, select on sequence api.recipes_id_seq to authenticated_user;
+
+grant usage on schema api to normal_user;
+
+grant select on api.ustensils to normal_user;
+grant all on api.ustensils to normal_user;
+
+grant usage, select on sequence api.recipes_id_seq to normal_user;
+grant all on api.recipes to normal_user;
+
+grant select on api.ustensils to normal_user;
+grant all on api.ustensils to normal_user;
+grant select, usage on api.ustensils_id_seq to normal_user;
+grant select on api.ustensils_recipe to normal_user;
+grant all on api.ustensils_recipe to normal_user;
+grant select, usage on api.ustensils_recipe_recipe_id_seq to normal_user;
+grant select, usage on api.ustensils_recipe_ustensil_id_seq to normal_user;
+
