f5fd50f275de916ba26d8cc7a71a87475116d453
James <me@j4m3s.eu>
2022-04-16 03:39:47 +0200

    feat(jms): add trailing whitespace function

--- projects/jms/src/jms/core.clj
+++ projects/jms/src/jms/core.clj
@@ -1,6 +1,27 @@
-(ns jms.core)
+(ns jms.core
+  (:require [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def lines
+  (str/split-lines "nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnntoto\ntata  \n"))
+
+(def lines-with-line-number
+  (let [lines-numbers (map inc (range (count lines)))]
+    (apply assoc {} (interleave lines-numbers lines))))
+
+; trailing whitespace lines
+(defn get-trailing-whitespace-lines [lines]
+  (filter
+   #(let [_line-number (first %) line-content (second %)]
+      (< 0 (count (re-seq #"\s+$" line-content))))
+   lines))
+
+(get-trailing-whitespace-lines lines-with-line-number)
+
+; default to 80
+(def max-line-character-count 80)
+
+(def lines-too-long
+  (filter #(> (count (second %)) max-line-character-count) lines-with-line-number))
+
+
+(map #(println "line " (first %) "is too long (" (count (second %)) " characters long).") lines-too-long)
