d9efe4149c5fb18d7a62866de4c69473f57f67e9
James <me@j4m3s.eu>
2022-04-17 16:52:32 +0200

    chore(jms): add test lines

--- projects/jms/test/jms/core_test.clj
+++ projects/jms/test/jms/core_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer [deftest is]]
             [jms.core :refer [has-windows-line-ending?]]))
 
+(def lines
+  (->
+   "nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnntoto\ntata  \n"
+   str/split-lines
+   ))
+
 (deftest test-has-windows-line-ending?
   (is (= (has-windows-line-ending? "toto\r\n") true)
       ))
