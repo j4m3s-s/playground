498e825a223a4bb3577a7ab7936ab875bb67503f
James <me@j4m3s.eu>
2022-04-19 18:36:31 +0200

    refactor(jms): move functions into jms.file

--- projects/jms/src/jms/core.clj
+++ projects/jms/src/jms/core.clj
@@ -4,45 +4,6 @@
   (:gen-class))
 
 
-; get lines with line-numbers
-(defn get-lines-with-line-number
-  [lines]
-  (let [lines-numbers (iterate inc 1)]
-    (zipmap lines-numbers lines)))
-
-; trailing whitespace lines
-(defn get-trailing-whitespace-lines
-  [lines]
-  (filter
-    #(let
-      [_line-number (first %)
-       line-content (second %)]
-      (< 0 (count (re-seq #"\s+$" line-content)))
-      lines)))
-
-; default to 80
-(def max-line-character-count 80)
-
-(defn get-lines-too-long
-  [lines-with-line-number]
-  (filter #(> (count (second %)) max-line-character-count) lines-with-line-number))
-
-(defn print-lines-too-long
-  [lines]
-  (map
-   #(let [line-number (:line-number %)
-          line-length (count (:line-content %))]
-      (println "line " line-number "is too long (" line-length " characters long)."))
-   lines))
-
-(defn remove-trailing-whitespace
-  [line]
-  (str/replace line #"\s+$" ""))
-
-(defn dos2unix-line
-  [line]
-  (str/replace line #"\r\n" "\n"))
-
 ; Entrypoint
 (defn -main [& args]
   (let [subcommand (first args)
--- projects/jms/src/jms/file.clj
+++ projects/jms/src/jms/file.clj
@@ -2,6 +2,45 @@
   (:require [clojure.string :as str])
   (:require [clojure.java.io :as io]))
 
+; get lines with line-numbers
+(defn get-lines-with-line-number
+  [lines]
+  (let [lines-numbers (iterate inc 1)]
+    (zipmap lines-numbers lines)))
+
+; trailing whitespace lines
+(defn get-trailing-whitespace-lines
+  [lines]
+  (filter
+    #(let
+      [_line-number (first %)
+       line-content (second %)]
+      (< 0 (count (re-seq #"\s+$" line-content)))
+      lines)))
+
+; default to 80
+(def max-line-character-count 80)
+
+(defn get-lines-too-long
+  [lines-with-line-number]
+  (filter #(> (count (second %)) max-line-character-count) lines-with-line-number))
+
+(defn print-lines-too-long
+  [lines]
+  (map
+   #(let [line-number (:line-number %)
+          line-length (count (:line-content %))]
+      (println "line " line-number "is too long (" line-length " characters long)."))
+   lines))
+
+(defn remove-trailing-whitespace
+  [line]
+  (str/replace line #"\s+$" ""))
+
+(defn change-dos2unix-line
+  [line]
+  (str/replace line #"\r\n" "\n"))
+
 (defn is-line-windows-line-ending?
   [line]
   (< 0
