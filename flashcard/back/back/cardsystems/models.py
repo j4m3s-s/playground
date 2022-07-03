ca01113687c35e64c097ea1d1a83fcaba418e7cd
James <me@j4m3s.eu>
2022-07-03 23:23:33 +0200

    feat(flashcard/back): make model appear nicer in admin

--- flashcard/back/back/cardsystems/models.py
+++ flashcard/back/back/cardsystems/models.py
@@ -6,10 +6,17 @@ from django.db import models
 class Tag(Model):
     name = models.CharField(max_length=30, unique=True)
 
+    def __str__(self):
+        return self.name
+
 class Card(Model):
     # FIXME: support for images / sound ?
     front = models.CharField(max_length=500)
     back = models.CharField(max_length=500)
+
+
+    def __str__(self):
+        return f'{self.front} - {self.back}'
 
 class CardTag(Model):
     card = models.ForeignKey(Card, on_delete=models.DO_NOTHING)
