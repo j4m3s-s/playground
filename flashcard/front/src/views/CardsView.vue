b6ab1624800dc1c356c142a64d747c0d2a1c3003
James <me@j4m3s.eu>
2022-07-04 14:54:41 +0200

    feat(flashcard): add boilerplate and cards list

---
+++ flashcard/front/src/views/CardsView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Cards' list</h1>
+    <ul>
+      <li v-for="item in items" :key="item.id">
+        {{ item.front }}
+        ----
+        {{ item.back }}
+        <div v-for="tag in item.tags" :key="tag.id"> #{{ tag }} </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import axios from 'axios'
+
+export default Vue.extend({
+  name: 'CardList',
+  data () {
+    return {
+      items: null
+    }
+  },
+  mounted () {
+    axios
+      .get('http://localhost:8000/api/v1/cardtags/?tag1=tata')
+      .then(response => (this.items = response.data))
+  }
+})
+</script>
