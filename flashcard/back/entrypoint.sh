dc6e44bc4c1e3d5ba065f3d86dffe1b9b597753e
James <me@j4m3s.eu>
2022-07-10 16:34:40 +0200

    feat(flashcard/back): add dockerfile

---
+++ flashcard/back/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -ex
+
+python manage.py makemigrations
+python manage.py migrate
+# FIXME: maybe needed in the future
+#python manage.py collectstatic --noinput
+
+# FIXME: wsgi or something ?
+exec python manage.py runserver
