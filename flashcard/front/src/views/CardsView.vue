40ded5070967f3b81c214c3556aa29f03ade6e3b
James <me@j4m3s.eu>
2022-07-17 17:03:31 +0200

    feat(flashcard/front): make cards auto refetch data

--- flashcard/front/src/views/CardsView.vue
+++ flashcard/front/src/views/CardsView.vue
@@ -18,6 +18,21 @@
 import Vue from 'vue'
 import axios from 'axios'
 
+async function fetchItems () {
+  let next = true
+  let count = 1
+  let results = []
+  while (next != null) {
+    const res = await axios
+      .get(`${process.env.VUE_APP_API_ENDPOINT_URL}/api/v1/cards?page=${count}`)
+    results = results.concat(...res.data.results)
+
+    next = res.data.next
+    count++
+  }
+  return results
+}
+
 export default Vue.extend({
   name: 'CardList',
   data () {
@@ -25,23 +40,22 @@ export default Vue.extend({
       items: null
     }
   },
+  // HACK: Called everytime this component is accessed
+  async setup () {
+    await this.fetchItems()
+  },
   methods: {
-    deleteCard (id) {
-      axios
-        .delete(`${process.env.VUE_APP_API_ENDPOINT_URL}/api/v1/card/${id}`).then((_) => {
-          // Refresh to re-retrieve items (and thus remove deleted one)
-          axios
-            .get(`${process.env.VUE_APP_API_ENDPOINT_URL}/api/v1/cards`)
-            // FIXME: paging
-            .then(response => (this.items = response.data.results))
-        })
+    async fetchItems () {
+      this.items = await fetchItems()
+    },
+    async deleteCard (id) {
+      await axios
+        .delete(`${process.env.VUE_APP_API_ENDPOINT_URL}/api/v1/card/${id}`)
+      await this.fetchItems()
     }
   },
-  mounted () {
-    axios
-      .get(`${process.env.VUE_APP_API_ENDPOINT_URL}/api/v1/cards`)
-      // FIXME: paging
-      .then(response => (this.items = response.data.results))
+  async mounted () {
+    await this.fetchItems()
   }
 })
 </script>
