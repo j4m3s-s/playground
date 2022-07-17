1ad2caf10e6cffda3784fe38c968d8325cc88e7b
James <me@j4m3s.eu>
2022-07-17 13:56:17 +0200

    feat(flashcard): use flake to define default envVar
    Signed-off-by: James <me@j4m3s.eu>

--- flake.nix
+++ flake.nix
@@ -13,6 +13,11 @@
   {
 
     devShell.${system} = pkgs.mkShell {
+      # Django default environment
+      DJANGO_ENV = "dev";
+      # VueJS api endpoint for dev (which is Django usually)
+      VUE_APP_API_ENDPOINT_URL = "http://localhost:8000";
+
       buildInputs = with pkgs; [
         gcc
         gnumake
