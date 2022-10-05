c8e5f5d47a5715ce88286c35007763f11c8e500a
James <me@j4m3s.eu>
2022-10-05 12:21:07 +0200

    feat(dns): add ExternalResourceRecord

--- projects/dns/src/main.rs
+++ projects/dns/src/main.rs
@@ -290,27 +290,141 @@ fn get_questions_vec(packet: &[u8], question_count: u16) -> Result<(Vec<Question
 
     let mut offset = mem::size_of::<ExternalDNSHeader>();
     for _ in 0..question_count {
-        let (question, offset) = get_question(packet, offset)?;
+        let (question, tmp_offset) = get_question(packet, offset)?;
+        offset = tmp_offset;
         vec.push(question);
     }
 
     Ok((vec, offset))
 }
 
-struct DNSPacket {
+struct DNSPacket<'buffer_life> {
     header: Header,
     questions: Vec<Question>,
+    additional_records: Vec<ExternalResourceRecord<'buffer_life>>
     // And more!
 }
 
+enum Name<'buffer_life> {
+    NonEmpty(&'buffer_life [u8]),
+    Empty,
+}
+
+#[derive(Debug)]
+struct ExternalResourceRecord<'buffer_life> {
+    name: &'buffer_life [u8],
+    rtype: u16,
+    class: u16,
+    ttl: u32,
+    rdlength: u16,
+    rdata: &'buffer_life [u8],
+}
+
+enum TypedResourceRecordData {
+    A([u8; 4]),
+    NS(String),
+    CNAME(String),
+    SOA, // TODO: implement me
+
+    // I'm trying to implement a DNS parser according to RFC1035 but a lot of other RFCs add new
+    // behaviors, especially EDNS-0's one (bigger DNS packets basically). So adding this escape
+    // hatch to be able to test things more easily.
+    UNKNOWN,
+}
+
+struct ResourceRecord {
+    name: String,
+    rtype: Type,
+    class: Class,
+    ttl: u32,
+}
+
+fn get_resource_record_vec(packet: &[u8], offset: usize) -> Result<(Vec<ExternalResourceRecord>,
+                                                                        usize), Error> {
+    let mut res = vec![];
+    let mut offset = offset;
+
+    let possible_null_byte: u8 = packet[offset];
+    let name: &[u8] = if possible_null_byte == 0 {
+        // empty
+        &packet[0..0]
+    } else {
+    // else parse something here
+        let mut sz = u16::from_be_bytes(packet[offset..offset + 2].try_into().unwrap());
+        let mut res = &packet[0..0];
+
+        let mask = 0xc000;
+        // we have a DNS "compressed" name
+        if sz & mask != 0 {
+            // we remove the mask, this makes an offset in the DNS packet
+            sz &= !mask;
+            let slice = &packet[(sz - 2) as usize..sz as usize];
+            let new_sz = u16::from_be_bytes(slice.try_into().unwrap());
+            res = &packet[sz as usize..sz as usize + new_sz as usize];
+
+            // We set size here as 1 since we skip the null byte later (which is not present here)
+            // and the size is actually a u16 (a size of data).
+            sz = 1;
+        }
+        offset += sz as usize;
+        res
+    };
+
+    // skip null byte
+    offset += 1;
+
+    let rtype = u16::from_be_bytes(packet[offset..offset + 2].try_into().unwrap());
+    println!("rtype {}", rtype);
+    offset += 2;
+    // FIXME: OPT
+    if rtype == 0x29 {
+        res.push(ExternalResourceRecord {
+            name: name,
+            rtype: rtype,
+            class: 0,
+            ttl: 0,
+            rdlength: 0,
+            rdata: &packet[0..0]
+        });
+        return Ok((res, offset));
+    }
+
+
+    let class = u16::from_be_bytes(packet[offset..offset + 2].try_into().unwrap());
+    offset += 2;
+
+    let ttl = u32::from_be_bytes(packet[offset..offset + 4].try_into().unwrap());
+    offset += 4;
+
+    let rdlength = u16::from_be_bytes(packet[offset..offset + 2].try_into().unwrap());
+    offset += 2;
+
+    let rdata = packet[offset..offset + rdlength as usize].try_into().unwrap();
+    offset += rdlength as usize;
+
+    res.push(ExternalResourceRecord {
+        name: name,
+        rtype: rtype,
+        class: class,
+        ttl: ttl,
+        rdlength: rdlength,
+        rdata: rdata,
+    });
+    Ok((res, offset))
+}
 fn get_parsed_dns_packet(packet: &[u8]) -> Result<DNSPacket, Error> {
     let header = get_header(packet).serialize()?;
 
     let (questions, offset) = get_questions_vec(packet, header.question_count)?;
 
+    let (additional, _) = get_resource_record_vec(&packet, offset)?;
+    println!("{:?}", additional[0]);
+
+
     Ok(DNSPacket {
         header: header,
         questions: questions,
+        additional_records: additional,
     })
 }
 
@@ -342,6 +456,60 @@ fn main() {
     let (questions, _) = get_questions_vec(bytes.as_slice().try_into().unwrap(), 1).unwrap();
     let q = &questions[0];
     println!("{:?} {:?} {:?}", q.qtype, q.qname, q.qclass);
+
+    let packet2 = byte_strings::concat_bytes!(
+        // transaction id
+        b"\x76\x11",
+
+        // Flags
+        b"\x01\x20",
+
+        // question len
+        b"\x00\x01",
+        // answer len
+        b"\x00\x00",
+        // authority len
+        b"\x00\x00",
+        // additional len
+        b"\x00\x01",
+
+        // Query (won't develop this one further since parsing it works
+        b"\x06\x67\x6f\x6f",
+        b"\x67\x6c\x65\x03\x63\x6f\x6d\x00\x00\x02\x00\x01",
+
+        // Additional records
+        // root record
+        b"\x00",
+        // OPT
+        b"\x00\x29",
+        // UDP payload size (in wireshark) which is not supposed to be here
+        b"\x04",
+        b"\xd0\x00\x00\x00\x00\x00\x0c\x00\x0a\x00\x08\xa5\x64\xcb\x63\xbe",
+        b"\x10\x0a\xf9");
+
+    let _ = get_parsed_dns_packet(packet2).unwrap();
+
+    let packet3 = byte_strings::concat_bytes!(
+        b"\x76\x11\x81\x00\x00\x01\x00\x00\x00\x04\x00\x09\x06\x67\x6f\x6f",
+        b"\x67\x6c\x65\x03\x63\x6f\x6d\x00\x00\x02\x00\x01\xc0\x0c\x00\x02",
+        b"\x00\x01\x00\x02\xa3\x00\x00\x06\x03\x6e\x73\x32\xc0\x0c\xc0\x0c",
+        b"\x00\x02\x00\x01\x00\x02\xa3\x00\x00\x06\x03\x6e\x73\x31\xc0\x0c",
+        b"\xc0\x0c\x00\x02\x00\x01\x00\x02\xa3\x00\x00\x06\x03\x6e\x73\x33",
+        b"\xc0\x0c\xc0\x0c\x00\x02\x00\x01\x00\x02\xa3\x00\x00\x06\x03\x6e",
+        b"\x73\x34\xc0\x0c\xc0\x28\x00\x1c\x00\x01\x00\x02\xa3\x00\x00\x10",
+        b"\x20\x01\x48\x60\x48\x02\x00\x34\x00\x00\x00\x00\x00\x00\x00\x0a",
+        b"\xc0\x28\x00\x01\x00\x01\x00\x02\xa3\x00\x00\x04\xd8\xef\x22\x0a",
+        b"\xc0\x3a\x00\x1c\x00\x01\x00\x02\xa3\x00\x00\x10\x20\x01\x48\x60",
+        b"\x48\x02\x00\x32\x00\x00\x00\x00\x00\x00\x00\x0a\xc0\x3a\x00\x01",
+        b"\x00\x01\x00\x02\xa3\x00\x00\x04\xd8\xef\x20\x0a\xc0\x4c\x00\x1c",
+        b"\x00\x01\x00\x02\xa3\x00\x00\x10\x20\x01\x48\x60\x48\x02\x00\x36",
+        b"\x00\x00\x00\x00\x00\x00\x00\x0a\xc0\x4c\x00\x01\x00\x01\x00\x02",
+        b"\xa3\x00\x00\x04\xd8\xef\x24\x0a\xc0\x5e\x00\x1c\x00\x01\x00\x02",
+        b"\xa3\x00\x00\x10\x20\x01\x48\x60\x48\x02\x00\x38\x00\x00\x00\x00",
+        b"\x00\x00\x00\x0a\xc0\x5e\x00\x01\x00\x01\x00\x02\xa3\x00\x00\x04",
+        b"\xd8\xef\x26\x0a\x00\x00\x29\x10\x00\x00\x00\x00\x00\x00\x00");
+
+    let _ = get_parsed_dns_packet(packet3).unwrap();
 }
 
 // These are just basic sanity tests. Much advanced testing (and maybe fuzzing) would be required to
