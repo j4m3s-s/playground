aadd9447d405006fda1b259d0d9514326ec234f9
James <me@j4m3s.eu>
2022-10-10 16:23:56 +0200

    feat(reshipi/models): add recipe step to admin

--- reshipi-django/back/back/admin.py
+++ reshipi-django/back/back/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from back.api.models import Recipe, Ingredient, Ustensil
+from back.api.models import Recipe, Ingredient, RecipeStep, Ustensil
 
 admin.site.register(Recipe)
 admin.site.register(Ingredient)
 admin.site.register(Ustensil)
+
+#FIXME: remove this and make it easy to edit into admin site directly using
+#Django shenanigans.
+admin.site.register(RecipeStep)
