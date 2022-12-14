1adf103c7280ae0d781c6919fbb42f60b82de74d
James <me@j4m3s.eu>
2022-12-14 15:13:23 +0100

    feat(clj/frontend): add working small cljs SPA

--- projects/clj/app/src/frontend/core.cljs
+++ projects/clj/app/src/frontend/core.cljs
@@ -1,10 +1,69 @@
 (ns frontend.core
   (:require [reagent.dom :as dom]
-            [re-frame.core :as rf]))
+            [re-frame.core :as rf]
+            [clojure.string :as str]))
+
+;; function dispatch event
+(defn dispatch-timer-event
+  []
+  (rf/dispatch (let [now (js/Date.)]
+                 [:timer now])))
+
+;; Timer call
+(defonce do-timer (js/setInterval dispatch-timer-event))
+
+;; Event handler
+(rf/reg-event-db
+ :initialize
+ (fn [_ _]
+   {:time (js/Date.)
+    :time-color "#f88"}))
+
+(rf/reg-event-db
+ :time-color-change
+ (fn [db [_ new-color]]
+   (assoc db :time-color new-color)))
+
+(rf/reg-event-db
+ :timer
+ (fn [db [_ new-time]]
+   (assoc db :time new-time)))
+
+;; Query
+
+(rf/reg-sub
+ :time
+ (fn [db _]
+   (:time db)))
+
+(rf/reg-sub
+ :time-color
+ (fn [db _]
+   (:time-color db)))
+
+(defn clock
+  []
+  [:div.example-clock
+   {:style {:color @(rf/subscribe [:time-color])}}
+   (-> @(rf/subscribe [:time])
+       .toString
+       (str/split " ")
+       first)])
+
+(defn color-input
+  []
+  [:div.color-input
+   "Time color:"
+   [:input {:type "text"
+            :value @(rf/subscribe [:time-color])
+            :on-change #(rf/dispatch [:time-color-change (-> % .-target .-value)])}]])
 
 (defn ui
   []
-  [:div {:style {:color "darkred"}} "Hello world" ])
+  [:div
+   [:h1 "hello !"]
+   [clock]
+   [color-input]])
 
 (defn render
   []
@@ -13,4 +72,5 @@
 
 (defn run
   []
+  (rf/dispatch-sync [:initialize])
   (render))
