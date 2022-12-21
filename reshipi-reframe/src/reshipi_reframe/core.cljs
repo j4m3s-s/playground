e42b3bdacb4c1eb0ec086d1d407d1703113dd91e
James <me@j4m3s.eu>
2022-12-21 02:56:41 +0100

    feat(reshipi-reframe): setup presentation scaffolding
    - Setup postcss configuration to generate files using TailwindCSS

--- reshipi-reframe/src/reshipi_reframe/core.cljs
+++ reshipi-reframe/src/reshipi_reframe/core.cljs
@@ -2,7 +2,6 @@
   (:require [reagent.dom :as dom]
             [re-frame.core :as rf]
             [clojure.string :as str]
-            [full.json :as json]
             [cljs-http.client :as http]
             [cljs.core.async :refer [<! go]]))
 
@@ -27,18 +26,109 @@
                        (get-in (<! (http/get "http://localhost:8000/api/v1/recipes")) [:body])]
   (rf/dispatch [:data data]))))
 
+
+(def example-recipe-list
+  [{:prep-time "00:00:10",
+     :name "toto",
+     :steps [{:id 1, :position 0, :text "ds"}],
+     :created "2022-12-16",
+     :modified "2022-12-16",
+     :cook-time "00:00:10",
+     :ingredients [{:id 1, :name "fff"}],
+     :ustensils [{:id 1, :name "fds"}],
+     :id 1,
+     :total-time "00:00:10",
+     :perform-time "00:00:10"}
+   {:prep-time "00:00:10",
+     :name "tata",
+     :steps [{:id 1, :position 0, :text "ds"}],
+     :created "2022-12-16",
+     :modified "2022-12-16",
+     :cook-time "00:00:10",
+     :ingredients [{:id 1, :name "fff"}],
+     :ustensils [{:id 1, :name "fds"}],
+     :id 1,
+     :total-time "00:00:10",
+     :perform-time "00:00:10"}
+   ])
+
+(def example-recipe-list
+  (first example-recipe-list))
+
+(defn component-step
+  [step]
+  (let [_id (:id step)
+        ; FIXME: sort by position
+        _position (:position step)
+        text (:text step)]
+  [:li text]))
+
+(defn component-steps
+  [steps]
+  [:div "Étapes :"
+   [:ul (map component-step steps)]])
+
+(defn component-ingredient
+  [ingredient]
+  (let [name (:name ingredient)]
+    [:li name ]))
+
+(defn component-ingredients
+  [ingredients]
+  [:div "Ingrédients :"
+   [:ul (map component-ingredient ingredients)]])
+
+(defn component-ustensil
+  [ustensil]
+  ; Currently it uses the same schema
+  (component-ingredient ustensil))
+
+(defn component-ustensils
+  [ustensils]
+  [:div "Ustensiles : "
+   [:ul (map component-ustensil ustensils)]])
+
+(defn component-recipe
+  [recipe]
+  (let [name (:name recipe)
+        _id (:id recipe)
+
+        created (:created recipe)
+        modified (:modified recipe)
+
+        cook-time (:cook-time recipe)
+        total-time (:total-time recipe)
+        perform-time (:perform-time recipe)
+
+
+        ingredients (:ingredients recipe)
+        steps (:steps recipe)
+        ustensils (:ustensils recipe)]
+    [:div
+     [:h1 name]
+     [:div "Temps de cuisson : " cook-time]
+     [:div "Temps total : " total-time]
+     [:div "Temps de préparation : " perform-time]
+
+     [:div "Créé le " created]
+     [:div "Dernière modification le " modified]
+
+     (component-ingredients ingredients)
+     (component-ustensils ustensils)
+     (component-steps steps)
+     ]))
+
 (defn ui
   []
   [:div
-   [:h1 "test"]
-   [:div "attttt"]
-   [:div @(rf/subscribe [:count])]
-   [:div (->
-          (get-in @(rf/subscribe [:data]) [:results])
-          first
-          :name
-          )
-          ]
+   ;[:div @(rf/subscribe [:count])]
+   ;[:div (->
+   ;       (get-in @(rf/subscribe [:data]) [:results])
+   ;       first
+   ;       :name
+   ;       )
+   ;       ]
+   [component-recipe example-recipe-list]
    ])
 
 (defn render
---
+++ reshipi-reframe/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "src/**/*",
+    "resources/public/index.html"
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
