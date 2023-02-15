361c7b5d9f0874ec70c1b34ac547b990758cce18
James <me@j4m3s.eu>
2023-02-15 19:10:24 +0100

    feat(projects): add b10s a Clojure tool for k8s
    The goal of the tool is to ensure compatibility while being able to use
    the data editing power of Clojure.

---
+++ projects/bb-k8s/src/script/main.clj
@@ -0,0 +1,66 @@
+(ns script.main
+  (:require [clojure.java.shell :refer [sh]]
+            [clj-yaml.core :as yaml]
+            [babashka.fs :as fs]
+            [clojure.edn :as edn]
+            [cheshire.core :as json]))
+
+(defn kustomize-build
+  [path]
+  (yaml/parse-string (:out (sh "kustomize" "build" path)) :load-all true))
+
+; Let's go simple :
+; - b10s.edn (just merge the result of this)
+;   Contains :kustomize [list of dirs]
+;            :b10s [list of dirs]
+;            :resources [{:apiVersion ...}]
+; - b10s.bb execs it and returns edn out of it
+
+(defn- k8s-merge-resources-top
+  [acc resources]
+  (if (empty? resources)
+    nil
+    (let [resource (first resources)
+          remain (rest resources)
+
+          kind (:kind resource)
+          name (get-in resource [:metadata :name])
+          namespace (get-in resource [:metadata :namespace])]
+      (if (contains? acc [kind name namespace])
+        (throw (Exception. (str "Error conflict on resource " name " in ns " namespace)))
+        (recur (conj acc [kind name namespace]) remain)))))
+
+; FIXME: what about non namespaced resources?
+(defn k8s-merge-resources
+  [resources]
+  (if (= nil (k8s-merge-resources-top #{} resources))
+    resources))
+
+
+(defn b10s-edn-exec
+  [path b10s-edn-path]
+  (let [cfg (edn/read-string (slurp b10s-edn-path))]
+    (k8s-merge-resources (flatten [(if (contains? cfg :kustomize)
+                            (kustomize-build (str path "/" (first (:kustomize cfg)))))
+                          (if (contains? cfg :resources)
+                            (:resources cfg))]))
+    ))
+
+(defn mk-k8s-list
+  "Generates a List manifest as k8s wants it"
+  [elements]
+  {:kind "List"
+   :apiVersion "v1"
+   :items elements
+   :metadata {:resourceVersion ""}})
+
+(defn b10s-exec
+  [path]
+  (let [b10s-edn-path (str path "/b10s.edn")]
+    (if (fs/exists? b10s-edn-path)
+      (json/generate-string (mk-k8s-list (b10s-edn-exec path b10s-edn-path))))
+      ))
+
+(defn -main [& args]
+  (let [path (first args)]
+    (println (b10s-exec path))))
