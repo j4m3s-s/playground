a6335baa2625bcdabfc01aea9c5dbddfdd43e138
James <me@j4m3s.eu>
2023-05-01 18:17:39 +0200

    chore(clj-web-base): remove unused imports

--- projects/clj-web-base/src/api/core.clj
+++ projects/clj-web-base/src/api/core.clj
@@ -1,13 +1,7 @@
 (ns api.core
   (:gen-class)
   (:require [io.pedestal.http :as http]
-            [io.pedestal.http.route :as route]
-            [environ.core :refer [env]]
-            [com.walmartlabs.lacinia.pedestal2 :as p2]
-
             ; Local imports
-            [api.schema :as schem]
-            [api.utils :refer [is-prod?]]
             [api.api :refer [service-map]]))
 
 (defn -main [& _] (-> service-map http/create-server http/start))
