08ad7f8f4a4058376cb6157fb3c5e1e17f0354af
James <me@j4m3s.eu>
2023-05-01 18:07:12 +0200

    refactor(clj-web-base): move data retrieval to db

---
+++ projects/clj-web-base/src/api/db.clj
@@ -0,0 +1,27 @@
+(ns api.db)
+
+(def blog-posts
+  {"0" {:id "0"
+      :content "Ho"
+      :title "ho"
+      :created 0
+      :tags ["ho"]}
+   "1" {:id "1"
+      :content "Ha"
+      :title "ha"
+      :created 1
+      :tags ["ha"]}})
+
+(defn resolve-blog-post
+  [_context args _value]
+  (blog-posts (:id args)))
+
+(defn resolve-blog-posts
+  [_context _args _value]
+  (map second blog-posts))
+
+(defn resolve-tags
+  [])
+
+(defn create-blog-post
+  [])
--- projects/clj-web-base/src/api/schema.clj
+++ projects/clj-web-base/src/api/schema.clj
@@ -2,43 +2,15 @@
   (:require [com.walmartlabs.lacinia.schema :as schema]
             [com.walmartlabs.lacinia.util :as util]
             [clojure.edn :as edn]
-            ))
-
-(def blog-posts
-  {"0" {:id "0"
-      :content "Ho"
-      :title "ho"
-      :created 0
-      :tags ["ho"]}
-   "1" {:id "1"
-      :content "Ha"
-      :title "ha"
-      :created 1
-      :tags ["ha"]}
-   })
-
-(defn resolve-blog-post
-  [_context args _value]
-  (blog-posts (:id args))
-  )
-
-(defn resolve-blog-posts
-  [_context _args _value]
-  (map second blog-posts))
-
-(defn resolve-tags
-  [])
-
-(defn create-blog-post
-  [])
+            [api.db :as db]))
 
 (defn blog-schema
   []
   ; FIXME: for prod use io/resources
   (-> (slurp "resources/blog-schema.edn")
       edn/read-string
-      (util/attach-resolvers {:resolve-blog-post resolve-blog-post
-                              :resolve-blog-posts resolve-blog-posts
-                              :resolve-tags resolve-tags
-                              :create-blog-post create-blog-post})
+      (util/attach-resolvers {:resolve-blog-post db/resolve-blog-post
+                              :resolve-blog-posts db/resolve-blog-posts
+                              :resolve-tags db/resolve-tags
+                              :create-blog-post db/create-blog-post})
       schema/compile))
