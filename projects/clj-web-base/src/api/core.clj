9e1e5e5baa29025adba47fc0f0337db5c8e56bbf
James <me@j4m3s.eu>
2023-05-01 18:01:07 +0200

    feat(clj-web-base): add graphiql / graphql setup
    Remove usage of default-service-map which makes custom config setup much
    harder.
    
    Some parts are not supported like subscriptions.

--- projects/clj-web-base/src/api/core.clj
+++ projects/clj-web-base/src/api/core.clj
@@ -1,9 +1,9 @@
 (ns api.core
   (:gen-class)
   (:require [io.pedestal.http :as http]
-            ;[io.pedestal.http.route :as route]
+            [io.pedestal.http.route :as route]
             ;[io.pedestal.http.body-params :as body-params]
-            ;[environ.core :refer [env]]
+            [environ.core :refer [env]]
             [com.walmartlabs.lacinia.pedestal2 :as p2]
             ;[clojure.java.io :as io]
 
@@ -11,31 +11,50 @@
             [api.schema :as schem]
   ))
 
-#_(def routes
-  [[["/" {:get `homepage}]
-    ["/graphql" {:post `schem/blog-schema}]
-    ;["/add-link" ^:interceptors [(body-params/body-params)] {:post `add-link}]
-    ]])
+(defn is-prod
+  []
+  (= (env :environment) "prod"))
+
+; Inspired from private pedestal2 (used in p2/default-service)
+; Can't really change them since it seems to be hardcoded somewhere
+(def ^:private default-asset-path "/assets/graphiql")
+(def ^:private default-api-path "/api")
+(def ^:private default-subscriptions-path "/ws")
+(def ^:private default-ide-path "/ide")
+
+; FIXME: graphql subscriptions are not enabled -- need more configuration
+(def ^:private default-jetty-options
+  {::http/type   :jetty
+   ::http/host   (or (env :host) "0.0.0.0")
+   ::http/join?  false
+   ::http/port (Integer. (or (env :port) 5000))
+
+   ;; CSP
+   ;; FIXME: use something more secure?
+   ;; NB: the nil for secure headers is needed for graphiql usage
+   ;; See lacinia-pedestal enable-graphiql.
+   ::http/secure-headers (if (is-prod)
+                           {:content-security-policy-settings "script-src 'unsafe-inline';"}
+                           nil)
+
+   ;; Jetty specific options
+   ;; see http://pedestal.io/reference/jetty
+   ::http/container-options {:h2c true}})
+(def ^:private interceptors (p2/default-interceptors schem/blog-schema {} default-jetty-options))
+
+(def routes
+  (into #{[default-api-path :post interceptors :route-name ::graphql-api]
+          ; FIXME not a good idea to keep this in prod -- removes security
+          [default-ide-path :get (p2/graphiql-ide-handler {}) :route-name ::graphql-ide]}
+        (p2/graphiql-asset-routes default-asset-path)))
 
 ; for production usage, use "export environment=prod" otherwise it expands
 ; routes on each request which destroys performance
-#_(def service-map {::http/routes (if (= (env :environment) "prod")
-                                  #(route/expand-routes routes)
-                                  (route/expand-routes routes))
-                  ::http/type   :jetty
-                  ::http/host   (or (env :host) "0.0.0.0")
-                  ::http/join?  false
-                  ::http/port (Integer. (or (env :port) 5000))
+(def service-map (merge default-jetty-options {::http/routes (if (is-prod)
+                                                               #(route/expand-routes routes)
+                                                               (route/expand-routes routes))}))
 
-                  ;; CSP
-                  ;; FIXME: use something more secure?
-                  ::http/secure-headers {:content-security-policy-settings "script-src 'unsafe-inline';"}
-
-                  ;; Jetty specific options
-                  ;; see http://pedestal.io/reference/jetty
-                  ::http/container-options {:h2c true}})
-
-(def service-map
+#_(def service-map
   (p2/default-service schem/blog-schema nil))
 
 (defn -main [& _] (-> service-map http/create-server http/start))
