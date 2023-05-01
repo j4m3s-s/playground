88a05699bd10a7e5f743bcd5f053d2e1d60f938f
James <me@j4m3s.eu>
2023-05-01 18:13:50 +0200

    refactor(clj-web-base): move api to api.clj

--- projects/clj-web-base/src/api/core.clj
+++ projects/clj-web-base/src/api/core.clj
@@ -8,46 +8,8 @@
             ; Local imports
             [api.schema :as schem]
             [api.utils :refer [is-prod?]]
+            [api.api :refer [service-map]]
   ))
-
-; Inspired from private pedestal2 (used in p2/default-service)
-; Can't really change them since it seems to be hardcoded somewhere
-(def ^:private default-asset-path "/assets/graphiql")
-(def ^:private default-api-path "/api")
-(def ^:private default-subscriptions-path "/ws")
-(def ^:private default-ide-path "/ide")
-
-; FIXME: graphql subscriptions are not enabled -- need more configuration
-(def ^:private default-jetty-options
-  {::http/type   :jetty
-   ::http/host   (or (env :host) "0.0.0.0")
-   ::http/join?  false
-   ::http/port (Integer. (or (env :port) 5000))
-
-   ;; CSP
-   ;; FIXME: use something more secure?
-   ;; NB: the nil for secure headers is needed for graphiql usage
-   ;; See lacinia-pedestal enable-graphiql.
-   ::http/secure-headers (if (is-prod?)
-                           {:content-security-policy-settings "script-src 'unsafe-inline';"}
-                           nil)
-
-   ;; Jetty specific options
-   ;; see http://pedestal.io/reference/jetty
-   ::http/container-options {:h2c true}})
-(def ^:private interceptors (p2/default-interceptors schem/blog-schema {} default-jetty-options))
-
-(def routes
-  (into #{[default-api-path :post interceptors :route-name ::graphql-api]
-          ; FIXME not a good idea to keep this in prod -- removes security
-          [default-ide-path :get (p2/graphiql-ide-handler {}) :route-name ::graphql-ide]}
-        (p2/graphiql-asset-routes default-asset-path)))
-
-; for production usage, use "export environment=prod" otherwise it expands
-; routes on each request which destroys performance
-(def service-map (merge default-jetty-options {::http/routes (if (is-prod?)
-                                                               #(route/expand-routes routes)
-                                                               (route/expand-routes routes))}))
 
 (defn -main [& _] (-> service-map http/create-server http/start))
 
