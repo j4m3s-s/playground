19d52383eeb4cf46023ba0d880958dac423a45d5
James <me@j4m3s.eu>
2023-05-01 18:17:02 +0200

    chore: apply cljfmt

--- projects/clj-web-base/src/api/core.clj
+++ projects/clj-web-base/src/api/core.clj
@@ -8,15 +8,14 @@
             ; Local imports
             [api.schema :as schem]
             [api.utils :refer [is-prod?]]
-            [api.api :refer [service-map]]
-  ))
+            [api.api :refer [service-map]]))
 
 (defn -main [& _] (-> service-map http/create-server http/start))
 
-#_ (def server (atom (->
-                      service-map
-                      http/create-server)))
-#_ @server
+#_(def server (atom (->
+                     service-map
+                     http/create-server)))
+#_@server
 
-#_ (http/start @server)
-#_ (http/stop @server)
+#_(http/start @server)
+#_(http/stop @server)
--- projects/clj-web-base/src/api/db.clj
+++ projects/clj-web-base/src/api/db.clj
@@ -2,15 +2,15 @@
 
 (def blog-posts
   {"0" {:id "0"
-      :content "Ho"
-      :title "ho"
-      :created 0
-      :tags ["ho"]}
+        :content "Ho"
+        :title "ho"
+        :created 0
+        :tags ["ho"]}
    "1" {:id "1"
-      :content "Ha"
-      :title "ha"
-      :created 1
-      :tags ["ha"]}})
+        :content "Ha"
+        :title "ha"
+        :created 1
+        :tags ["ha"]}})
 
 (defn resolve-blog-post
   [_context args _value]
