c85248a38cf6d82eb1d5f2c38a91ddf3e67a1ae1
James <me@j4m3s.eu>
2023-05-03 00:24:32 +0200

    feat(clj-web-base): add basic xtdb support

--- projects/clj-web-base/src/api/core.clj
+++ projects/clj-web-base/src/api/core.clj
@@ -2,9 +2,13 @@
   (:gen-class)
   (:require [io.pedestal.http :as http]
             ; Local imports
-            [api.api :refer [service-map]]))
+            [api.api :refer [service-map]]
+            [api.db :as db]
+            [xtdb.api :as xt]))
 
 (defn -main [& _] (-> service-map http/create-server http/start))
+
+; NOTABENE: xtdb is currently launched when the xtdb ns is imported.
 
 #_(def server (atom (->
                      service-map
@@ -13,3 +17,76 @@
 
 #_(http/start @server)
 #_(http/stop @server)
+
+#_(def xtdb-node (atom (db/start-xtdb!)))
+#_(do (.close @xtdb-node)
+      (reset! xtdb-node nil))
+
+; XTDB
+#_(xt/submit-tx @xtdb-node [[::xt/put
+                             {:xt/id "hi2u"
+                              :user/name "zig"}]])
+
+#_(xt/q (xt/db @xtdb-node) '{:find [e]
+                            :where [[e :user/name "zig"]]})
+
+#_ (xt/submit-tx @xtdb-node [[::xt/put
+                              {:xt/id (java.util.UUID/randomUUID)
+                               :type :tag
+                               :name "ha"}]])
+
+#_ (xt/q (xt/db @xtdb-node)
+        '{:find [(pull ?e [*])]
+          :where [[?e :type :tag]
+                   ]})
+
+#_(xt/submit-tx @xtdb-node [[::xt/put
+                              {:xt/id :test
+                               :content "Ha"
+                               :title "ha"
+                               :created 12
+                               :tags (letfn [(search-tag [name] (xt/q (xt/db @xtdb-node) '{:find [id]
+                                                                                           :in [name]
+                                                                                           :where [[e :xt/id id]
+                                                                                                   [e :type :tag]
+                                                                                                   [e :name name]]}
+                                                                      name))]
+                                       (map search-tag #{"ha" "ho"}))}
+                              ]])
+
+#_(xt/sync @xtdb-node)
+
+
+#_(xt/q (xt/db @xtdb-node)
+        '{:find [(pull ?bp [:tags])]
+          :where [[?bp :xt/id :test]
+                   ]})
+
+#_(xt/q (xt/db @xtdb-node)
+        '{:find [title content tags-name]
+          :where [[?bp :xt/id :test]
+                  [?bp :title title]
+                  [?bp :content content]
+                  [?bp :tags tags]
+                  [(q
+                    {:find [name]
+                     :where [[?e :name name]
+                             [?e :type :tag]
+                             [?e :xt/id ?id]
+                             [?bp :tags ?id]
+                             ]
+                     :in [?bp]
+                     }
+                    ?bp) tags-name]
+                   ]})
+
+#_(xt/q (xt/db @xtdb-node)
+        '{:find [?tag]
+          :where [[?tag :name "ho"]
+                  [?bp :tags ?tag]
+                   ]})
+
+
+
+#_(xt/entity (xt/db @xtdb-node) "1")
+
--- projects/clj-web-base/src/api/db.clj
+++ projects/clj-web-base/src/api/db.clj
@@ -1,4 +1,19 @@
-(ns api.db)
+(ns api.db
+  (:require [clojure.java.io :as io]
+            [xtdb.api :as xt]))
+
+(defn start-xtdb!
+  []
+  (letfn [(kv-store [dir]
+            {:kv-store {:xtdb/module 'xtdb.rocksdb/->kv-store
+                        :db-dir (io/file dir)
+                        :sync true}})]
+    (xt/start-node
+     {:xtdb/tx-log (kv-store "data/dev/tx-log")
+      :xtdb/document-store (kv-store "data/dev/doc-store")
+      :xtdb/index-store (kv-store "data/dev/index-store")})))
+
+(def db (atom start-xtdb!))
 
 (def blog-posts
   {"0" {:id "0"
@@ -14,7 +29,16 @@
 
 (defn resolve-blog-post
   [_context args _value]
-  (blog-posts (:id args)))
+  (let [id (:id args)]
+    (xt/q
+     @db
+     '{:find [title content created tags]
+       :in [id]
+       :where [[id :title title]
+               [id :content content]
+               [id :created created]
+               [id :tag tags]]}
+     id)))
 
 (defn resolve-blog-posts
   [_context _args _value]
