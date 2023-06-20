ba4b6a417de5847b9540861eb67e1a6648a20624
James <me@j4m3s.eu>
2023-06-20 23:44:36 +0200

    refactor(yt-music): use myLib build functions
    Apparently this wasn't cleaned-up properly.
    
    Move build to new functions also fixed some infinite recursion problem
    that I don't really understand. Probably something related to derivation
    being an attrset that contains a reference to a parent attrset.

--- projects/yt-music/default.nix
+++ projects/yt-music/default.nix
@@ -3,7 +3,10 @@
 with pkgs.lib;
 
 let
-  stdenv = pkgs.stdenv;
+  inherit (myLib)
+    mkJar
+    mkNativeFromJar
+  ;
 
   gitignoreSource = repo.gitignoreSource;
   deps = import ./deps.nix {
@@ -20,78 +23,12 @@ let
 
   projectSrc = builtins.filterSource (path: _type: path != "default.nix") ./.;
 
-  mkJar = name: mainClass:
-    with pkgs;
-    #assert (hasSuffix ".jar" name);
-    stdenv.mkDerivation rec {
-      inherit name;
-      dontUnpack = true;
-      buildPhase = ''
-        export HOME=$(pwd)
-        cp -rf ${projectSrc}/* .
-        ${clojure}/bin/clojure \
-          -Scp ${classpath.prod} \
-          -M:uberjar \
-          ${name} \
-          -C -m ${mainClass}
-      '';
-
-      doCheck = true;
-
-      checkPhase = ''
-        echo "checking for existence of ${name}"
-        [ -f ${name} ]
-      '';
-
-      installPhase = ''
-        cp ${name} $out
-      '';
-    };
-
-  mkNativeFromJar = name: entryJar: reflectionConfig:
-    stdenv.mkDerivation rec {
-      inherit name;
-
-      dontUnpack = true;
-      # ReportExceptionStackTraces : to get an idea of where reflection is used
-      # no-fallback : don't build a slow image is reflection is used
-      # Reflection configuration contains class to load that use reflection
-      buildPhase = ''
-        ${pkgs.graalvm17-ce}/bin/native-image \
-        -cp ${classpath.prod} \
-        -jar ${entryJar} \
-        ${name} \
-        --initialize-at-build-time \
-        -H:+ReportExceptionStackTraces \
-        -H:ReflectionConfigurationFiles=${reflectionConfig} \
-        --no-fallback
-      '';
-
-      doCheck = true;
-      checkPhase = ''
-        echo "checking for existence of ${name}"
-        [ -f ${name} ]
-        ./${name}
-      '';
-
-      installPhase = ''
-        cp ${name} $out
-      '';
-    };
-
   ytMusicCliPkgs = with pkgs; [
     yt-dlp
     chromaprint # for fpcalc fingerprint calculation
     id3v2
   ];
-in
-rec {
-  shell = myLib.mkShell {
-    buildInputs = ytMusicCliPkgs;
-    meta.ci.build = true;
-  };
-  jar = mkJar "yt-music.jar" "yt-music.core";
-  bin = mkNativeFromJar "yt-music" jar ./reflect-cfg.json;
+
   img-container = nix2container.buildImage {
     name = "yt-music";
     #config.cmd = ["${pkgs.jdk11}/bin/java -jar ${jar}"];
@@ -113,4 +50,38 @@ rec {
     ];
   };
   container-script = myLib.mkOCIUploadScript img-container "j4m3s/yt-music" version;
+  src = ./.;
+  mainClass = "yt-music.core";
+  jar = mkJar {
+    name = "yt-music.jar";
+    inherit
+      mainClass
+      classpath
+      src
+    ;
+    # Currently build is broken
+    meta.ci.build = false;
+  };
+in
+{
+  bin = mkNativeFromJar {
+    name = "bin";
+    entryJar = jar;
+    reflectionConfig = ./reflect-cfg.json;
+    inherit
+      classpath
+    ;
+    # Currently jar build is broken
+    meta.ci.build = false;
+  };
+  shell = myLib.mkShell {
+    buildInputs = ytMusicCliPkgs;
+    meta.ci.build = true;
+  };
+  #bin = mkNativeFromJar "yt-music" jar ./reflect-cfg.json;
+  inherit
+    #container-script
+    #img-container
+    jar
+  ;
 }
