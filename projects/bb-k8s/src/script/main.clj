6baa238180b870ceffdc20764c32804da1bdf211
James <me@j4m3s.eu>
2023-06-25 14:49:11 +0200

    feat(b10s): remove unused part of the software

--- projects/bb-k8s/src/script/main.clj
+++ projects/bb-k8s/src/script/main.clj
@@ -10,13 +10,6 @@
   [path]
   (yaml/parse-string (:out (sh "kustomize" "build" path)) :load-all true))
 
-; Let's go simple :
-; - b10s.edn (just merge the result of this)
-;   Contains :kustomize [list of dirs]
-;            :b10s [list of dirs]
-;            :resources [{:apiVersion ...}]
-; - b10s.bb execs it and returns edn out of it
-
 (defn- k8s-merge-resources-top
   [acc resources]
   (if (empty? resources)
@@ -37,20 +30,6 @@
   (if (= nil (k8s-merge-resources-top #{} resources))
     resources))
 
-(defn b10s-edn-exec
-  [path b10s-edn-path]
-  (let [cfg (edn/read-string (slurp b10s-edn-path))]
-    (k8s-merge-resources (flatten [
-                                   (when (contains? cfg :kustomize)
-                                     (kustomize-build (str path "/" (first (:kustomize cfg)))))
-                                   (when (contains? cfg :resources)
-                                     (:resources cfg))
-                                   (when (contains? cfg :b10s)
-                                     (eval (read-string
-                                       (slurp (str path "/" (first (:b10s cfg)) "/b10s.bb")))))
-                                   ]))
-    ))
-
 (defn mk-k8s-list
   "Generates a List manifest as k8s wants it"
   [elements]
@@ -59,12 +38,6 @@
    :items elements
    :metadata {:resourceVersion ""}})
 
-(defn b10s-exec
-  [path]
-  (let [b10s-edn-path (str path "/b10s.edn")]
-    (when (fs/exists? b10s-edn-path)
-      (json/generate-string (mk-k8s-list (b10s-edn-exec path b10s-edn-path))))
-      ))
 
 (defn -main [& args]
   (let [; We assume it's used as kustomize build ...
